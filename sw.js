2b40b765adcae630b1c48141b0f6d5120bfe0cef
LuisJallo <luisjallo12@gmail.com>
2026-08-16 18:54:24 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
-
-const CACHE_NAME = 'conexzion-v1';
-const urlsToCache = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+const CACHE = 'conexzion-v4';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request).then(f=>{
+        // don't cache api calls
+        if(e.request.url.includes('chrome-extension')) return f;
+        return f;
+      }).catch(()=> caches.match('/index.html'));
+    })
+  );
 });
